b4c5fbce3cd1fc2e853aca299b97ae33cdbf29f8
bw <4862abd@gmail.com>
2024-10-28 23:31:59 +0900

    [SYSTEM] Init SpringBootAwsSoloApplication

---
+++ src/test/java/com/example/springbootawssolo/SpringBootAwsSoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootawssolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsSoloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
