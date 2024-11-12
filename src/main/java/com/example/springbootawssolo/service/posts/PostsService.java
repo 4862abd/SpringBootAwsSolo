f40c4fb9ba9d1b66f41f32c710a070100702f6a3
bw <4862abd@gmail.com>
2024-11-12 16:37:12 +0900

    [04Mustache - End] CREATE classes, htmls, UPDATE schema, ADD build.gradle dependency, ADD properties

--- src/main/java/com/example/springbootawssolo/service/posts/PostsService.java
+++ src/main/java/com/example/springbootawssolo/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.springbootawssolo.service.posts;
 
 import com.example.springbootawssolo.domain.posts.Posts;
 import com.example.springbootawssolo.domain.posts.PostsRepository;
+import com.example.springbootawssolo.web.dto.PostListResponseDto;
 import com.example.springbootawssolo.web.dto.PostsResponseDto;
 import com.example.springbootawssolo.web.dto.PostsSaveRequestDto;
 import com.example.springbootawssolo.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
     }
 }
--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,1 +1,1 @@
-insert into posts (author, content, title) values ('author', 'content', 'title');
+insert into posts_springboot_aws_solo (author, content, title) values ('author', 'content', 'title');
