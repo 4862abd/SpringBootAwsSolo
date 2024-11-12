9e696abff15d317b612ed2c9d0a4d63b0f7fd68e
bw <4862abd@gmail.com>
2024-11-12 22:08:53 +0900

    [05OAuth - Ing] ADD files for OAuth2 - Naver

--- src/main/java/com/example/springbootawssolo/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/springbootawssolo/config/auth/dto/OAuthAttributes.java
@@ -29,7 +29,9 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of (String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        System.out.println("registrationId: " + registrationId);
         if ("naver".equals(registrationId)) {
+            System.out.println("ㅇㅇ");
             return ofNaver("id", attributes);
         }
 
--- src/main/java/com/example/springbootawssolo/config/auth/dto/SessionUser.java
+++ src/main/java/com/example/springbootawssolo/config/auth/dto/SessionUser.java
@@ -3,11 +3,13 @@ package com.example.springbootawssolo.config.auth.dto;
 import com.example.springbootawssolo.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 /**
  * 인증된 사용자 정보만
  */
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
 
     private String name;
     private String email;
